0b597078e6ad404093787141a4f47f4c45d37a53
heyazoo1007 <heyazoo1007@gmail.com>
2022-11-19 17:33:47 +0900

    [FIX] 파일 구조 수정

--- src/main/java/com/example/zerobase_part1/etc/HelloApplication.java
+++
@@ -1,9 +0,0 @@
-package com.example.zerobase_part1.etc;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class HelloApplication extends Application {
-
-}
